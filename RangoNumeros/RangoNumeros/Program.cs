19e654432c588a7f2bdbd478770ce04ddc3802c6
unknown <navgil63@gmail.com>
2024-05-30 21:43:44 -0600

    Subida de practica en consola RangoNumeros

---
+++ RangoNumeros/RangoNumeros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RangoNumeros
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el valor para x: ");
+            int x = Convert.ToInt32(Console.ReadLine()); //Almacena una entrada de datos
+
+            Console.WriteLine("Ingresa el valor de y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i <= y; i++)
+            {
+                Console.Write("{0} ",i);//Se llama al nombre del archivo despues el metodo
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
