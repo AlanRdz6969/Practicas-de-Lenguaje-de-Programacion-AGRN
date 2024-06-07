f1bc136ebb7a942a3833baf98e97493fe0c097ad
unknown <navgil63@gmail.com>
2024-06-06 20:37:55 -0600

    Subida del Proyecto de consola MayorDeDosNumeros

---
+++ MayorDeDosNumeros/MayorDeDosNumeros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MayorDeDosNumeros
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el Primer Valor: ");
+            int PrimerValor = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Ingrese el Segundo Valor: ");
+            int SegundoValor = Convert.ToInt32(Console.ReadLine());
+
+            if (PrimerValor > SegundoValor)
+            {
+                Console.WriteLine("El Numero Mayor es: " + PrimerValor);
+            }
+
+            else if (SegundoValor > PrimerValor)
+            {
+                Console.WriteLine("eL Numero Mayor es: " +  SegundoValor);
+            }
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
