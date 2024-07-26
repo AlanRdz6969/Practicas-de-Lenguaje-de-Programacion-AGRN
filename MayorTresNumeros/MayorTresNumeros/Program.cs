37102e1898a79c32423f4245b055ac5836b62480
unknown <navgil63@gmail.com>
2024-07-25 21:44:31 -0600

    Modificacion codigo MayorDeTres

--- MayorTresNumeros/MayorTresNumeros/Program.cs
+++ MayorTresNumeros/MayorTresNumeros/Program.cs
@@ -7,36 +7,39 @@ namespace MayorTresNumeros
         static void Main(string[] args)
         {
             Console.WriteLine("Ingrese un valor para X: ");
-            int x = Convert .ToInt32(Console.ReadLine());
+            int x = Convert.ToInt32(Console.ReadLine());
 
             Console.Clear();
             Console.WriteLine("Ingrese un valor para y: ");
-            int y = Convert .ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
 
             Console.Clear();
             Console.WriteLine("Introduzca un valor para z: ");
-            int z = Convert .ToInt32(Console.ReadLine());
-            if (x > y && x > z)
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            int mayor = x;
+            if (y > mayor) mayor = y;
+            if (z > mayor) mayor = z;
+
+
+            switch (mayor)
             {
-                Console.Clear();
-                Console.WriteLine("El numero Mayor es: " + x);
-            }
-            else if (y > x && y > z)
-            {
-                Console.Clear();
-                Console.WriteLine("El numero Mayor es: " + y);
-            }
-            else if (z > x && z > y)
-            {
-                Console.Clear();
-                Console.WriteLine("El numero Mayor es: " + z);
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("El numero Mayor es: " + x);
+                case int n when (n == x && x > y && x > z):
+                    Console.WriteLine("El numero Mayor es: " + x);
+                    break;
+                case int n when (n == y && y > x && y > z):
+                    Console.WriteLine("El numero Mayor es: " + y);
+                    break;
+                case int n when (n == z && z > x && z > y):
+                    Console.WriteLine("El numero Mayor es: " + z);
+                    break;
+                default:
+                    Console.WriteLine("Hay dos o más números iguales o algo inesperado.");
+                    break;
+
             }
             Console.ReadKey();
         }
+
     }
 }
